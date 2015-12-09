c6661270b8da594b91bfb452eb1f76fc054972bc
João Victor Magela <joao.dk16@gmail.com>
2015-12-09 12:39:16 -0200

    Log
    Download dos logs de teste

--- js/Globals.js
+++ js/Globals.js
@@ -1,9 +1,11 @@
-//Conjunto de variáveis globais
+//Conjunto de variáveis é funções globais
 'use strict';
 var globalTree = null;
 var abstractInterfaceObj;
 var concreteInterfaceObj;
 var messagesLog = [];
+
+//Executa os testes
 var runTest = function(){
     Project.modal.showPleaseWait();
     Project.tests();
@@ -18,5 +20,38 @@ var runTest = function(){
     setTimeout(function() {
         Project.modal.hidePleaseWait();
         $('#modal-test').modal('show');
+        downloadLog(generateLog());
     }, 7000);
 };
+
+var generateLog = (function(){
+	var text = '';
+
+	for(var i = 0; i < messagesLog.length; i++) {
+		text += messagesLog[i].module + " - " +  messagesLog[i].name + "\n";
+		text += "Status: " + (messagesLog[i].result ? "Passou" : "Não passou") + "\n";
+		text += "Assertiva: " + messagesLog[i].message + "\n";
+		text += "\n";
+	}
+
+	return text;
+})
+
+//Faz o download do log gerado
+var downloadLog = function(text){
+	var file = new Blob([text], {type: 'text/plain'})
+	var url = URL.createObjectURL(file);
+	var element = document.createElement('a');
+	element.setAttribute('href', url);
+	element.setAttribute('download', 'Mira-editor-log');
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+};
+
+//Evento do QUnit
+QUnit.log(function(details){
+	messagesLog.push(details);
+});
+
