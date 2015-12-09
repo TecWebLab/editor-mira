a04ad36b9c0ead0c12d24e9bcd07ae58f490fdbb
João Victor Magela <joao.dk16@gmail.com>
2015-12-09 09:46:55 -0200

    Variáveis globais
    Mudança da localização de variáveis globais

--- js/Globals.js
+++ js/Globals.js
@@ -3,3 +3,20 @@
 var globalTree = null;
 var abstractInterfaceObj;
 var concreteInterfaceObj;
+var messagesLog = [];
+var runTest = function(){
+    Project.modal.showPleaseWait();
+    Project.tests();
+    RenderElements.testsTreeView();
+    UIEvents.tests();
+    Sidebar.tests();
+    setTimeout(function() {
+        Code.tests();
+        Preview.tests();
+    }, 4000);
+
+    setTimeout(function() {
+        Project.modal.hidePleaseWait();
+        $('#modal-test').modal('show');
+    }, 7000);
+};
--- js/Init.js
+++ js/Init.js
@@ -2,22 +2,7 @@ $(document).ready(function () {
     globalTree = null;
     abstractInterfaceObj = new AbstractInterface();
     concreteInterfaceObj = new ConcreteInterface();
-    var runTest = function(){
-        Project.modal.showPleaseWait();
-        Project.tests();
-        RenderElements.testsTreeView();
-        UIEvents.tests();
-        Sidebar.tests();
-        setTimeout(function() {
-            Code.tests();
-            Preview.tests();
-        }, 4000);
-
-        setTimeout(function() {
-            Project.modal.hidePleaseWait();
-            $('#modal-test').modal('show');
-        }, 7000);
-    };
+    
 
     $('#content-templates').load('templates.html', function () {
         RenderElements.initRenderElements();
