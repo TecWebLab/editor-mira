7d717f597994530695e1d1b0087e904b6232a766
João Victor Magela <magela@tecgraf.puc-rio.br>
2017-04-10 14:17:59 -0300

    Template
    Criação do template para geração das interfaces do Mira

--- js/appInterfaces.js
+++ js/appInterfaces.js
@@ -14,6 +14,9 @@ var AppInterfaces = function(){
 	this.$process = $("#btn-process");
 	this.$file = $("#input-json");
 	this.$txtFile = $("#text-file");
+	this.$codeModel = $("#pre-model");
+	this.$codeMira = $("#pre-mira");
+
 }
 
 /*
@@ -21,20 +24,79 @@ var AppInterfaces = function(){
 
 */
 AppInterfaces.prototype.Init = function() {
-	
+	this.InitEvents();
 };
 
 AppInterfaces.prototype.InitEvents = function() {
 	var _this = this;
 
+	this.$file.change(function(e){
+		var filename = _this.$file.val().split('\\').pop();
+		_this.$txtFile.val(filename);
+	});
+
 	this.$process.click(function(e){
 		e.preventDefault();
 
-		//TODO: validar o arquivo
-		_this.ProcessFile(file);
+		var files = _this.$file[0].files;
+
+		if(files.lenght == 0)
+			alert ("Nenhum arquivo foi selecionado.");
+
+		if(files.lenght > 1)
+			alert("Mais de um arquivo foi selecionado.");
+		
+		_this.ProcessFile(files[0]);
 	});
 };
 
 AppInterfaces.prototype.ProcessFile = function(file) {
-	// body...
+	var _this = this;
+	var reader = new FileReader();
+	reader.onload = (function(fileAsText) {
+		return function(e){
+			var import = JSON.parse(e.target.result, function(key, value){
+				//Converte o validation de string para function
+				return key == "validation" ? new Function(value) : value;
+			});
+
+			//Exibe o json importado
+			_this.$codeModel.html(e.target.result);
+
+			//Exibe as interfaces do Mira de acordo com o template
+
+			//Cadastra os dados no API.ai
+		}
+	})(file);
+
+	reader.readAsText(file);
 };
+
+AppInterfaces.prototype.CreateMiraInterfaces = function(import) {
+
+};
+
+AppInterfaces.AppInterfaces.prototype.GetDeepPropertiesValue = function(widgtes){
+    var titles = [];
+    var findName = function(list){
+    	if(_.isString(list)){
+    		titles.push(list);
+    	}
+    	else{
+    		for(var key in list){
+	            if(_.isString(list[key].name) && list[key].name.length)
+	                titles.push(list[key].name);
+
+	            
+	            if(typeof(list[key].children) != 'undefined'){
+	            	if(list[key].children)
+	            	findName(list[key].attributes.children.models);
+	            }
+	            
+	        }	
+    	}
+    };
+
+    findName(widgtes);
+    return titles;
+}
